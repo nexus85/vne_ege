0fbcd1d626c525d2f78cfb314b615deaa7bf913e
nexus <mx38@mail.ru>
2021-01-02 14:28:31 +0300

    libraty add

---
+++ my_house.py
@@ -0,0 +1,14 @@
+from graph import *
+
+def paint_house(x, y, width, height):
+    pass
+    print('LOG: paint_house() called with parameters: ', 
+    x,y, width, height)
+
+
+def main():
+    paint_house(150, 150, 120, 100)
+    run()
+
+
+main()
